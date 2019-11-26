b02751055a6bd4258e64abfc6a771a556b90c37e
Austin <austinobejo@gmail.com>
2019-11-26 01:27:48 -0500

    Fixed error in how the server returns json files

--- src/main/java/darkpurple/hw2/controller/RecipeController.java
+++ src/main/java/darkpurple/hw2/controller/RecipeController.java
@@ -1,43 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package darkpurple.hw2.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import darkpurple.hw2.database.CustomUserDetailsService;
 import darkpurple.hw2.database.RecipeService;
 import darkpurple.hw2.database.entity.Recipe;
 import darkpurple.hw2.database.entity.User;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author anilramsoomye
- */
-@Controller
+@RestController
 public class RecipeController {
-    
+
     @Autowired
     private RecipeService recipeService;
-    
+
     @Autowired
     private CustomUserDetailsService userService;
-   
 
-   
     @RequestMapping(value = "/recipe/get", method = RequestMethod.GET)
     public Recipe getRecipe(@RequestBody String recipeId) {
         return recipeService.findRecipeById(recipeId);
-        
+
     }
-    
+
     @RequestMapping(value = "/recipe/add", method = RequestMethod.POST)
     public Recipe addRecipe(@RequestBody String name, String glass, String[] ingredients, float[] volumes) {
         Recipe recipe = new Recipe();
@@ -52,15 +47,25 @@ public class RecipeController {
 
         return recipe;
     }
-    
-    
+
     @RequestMapping(value = "/recipe/delete", method = RequestMethod.POST)
     public void deleteRecipe(@RequestBody String recipeId) {
         recipeService.deleteRecipe(recipeId);
     }
-    
-    @RequestMapping(value = "/recipe/list", method = RequestMethod.GET)
-    public List<Recipe> findAllRecipes() {
-        return recipeService.getAllRecipes();
+
+    @RequestMapping(value = "/recipe/list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String findAllRecipes() {
+        
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            Map outputMap = new HashMap();
+            List<Recipe> recipeList = recipeService.getAllRecipes();
+            outputMap.put("recipes", recipeList);
+            String output = mapper.writeValueAsString(outputMap);
+            return output;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "Error";
+        }
     }
 }
