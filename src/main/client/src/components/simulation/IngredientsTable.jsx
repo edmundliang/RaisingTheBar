7891d459fc50b8a0ad41b7a21aedb043bbe4d79e
Austin <austinobejo@gmail.com>
2019-11-26 13:15:32 -0500

    Connected ingredients to remove database

--- src/main/client/src/components/simulation/IngredientsTable.jsx
+++ src/main/client/src/components/simulation/IngredientsTable.jsx
@@ -7,17 +7,8 @@ export default class IngredientsTable extends Component {
 		super(props);
 
 		this.state = {
-			categoryList: [],
-			selected: null,
-			ingredientsList: new Map()
+			selected: null
 		};
-		for (var x of props.ingredients) {
-			if (this.state.ingredientsList.get(x["category"]) == null) {
-				this.state.ingredientsList.set(x["category"], []);
-			}
-			this.state.ingredientsList.get(x["category"]).push(x);
-		}
-		this.state.ingredientsList.forEach((value, key, map) => { this.state.categoryList.push(key) })
 	}
 	onListElementClick(item, event) {
 		if (item !== this.state.selected) {
@@ -27,20 +18,32 @@ export default class IngredientsTable extends Component {
 		}
 	}
 	render() {
+
+		let categoryList = [];
+		let ingredientsList = new Map();
+		if (this.props.ingredients != null) {
+			for (var x of this.props.ingredients) {
+				if (ingredientsList.get(x["type"]) == null) {
+					ingredientsList.set(x["type"], []);
+				}
+				ingredientsList.get(x["type"]).push(x);
+			}
+			ingredientsList.forEach((value, key, map) => { categoryList.push(key) })
+		}
 		return (
 
-			<Tabs defaultActiveKey={this.state.categoryList[0]}>
+			<Tabs defaultActiveKey={categoryList[0]}>
 				{
-					this.state.categoryList.map((item) => (
+					categoryList.map((item) => (
 						<Tab key={item} eventKey={item} title={item}>
 							<div className="flex-container">
 								{(() => {
 									var output = [];
-									var elements = this.state.ingredientsList.get(item)
+									var elements = ingredientsList.get(item)
 									for (var x of elements) {
 										let boundFunctionCall = this.onListElementClick.bind(this, x);
 										output.push(
-											<div className={this.state.selected === x ? "list_element selected" : "list_element"}  key={x["name"]} onClick={boundFunctionCall}>{x["name"]}</div>
+											<div className={this.state.selected === x ? "list_element selected" : "list_element"} key={x["name"]} onClick={boundFunctionCall}>{x["name"]}</div>
 										);
 									}
 									return output;
