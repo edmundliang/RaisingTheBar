b7489debab5723f1ee2279f3f8e5a65774616f70
Austin <austinobejo@gmail.com>
2019-11-25 23:46:01 -0500

    Fix small typo in import

--- src/main/client/src/components/workshop/Workshop.jsx
+++ src/main/client/src/components/workshop/Workshop.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import NavigationBar from "./../navbar/NavigationBar";
 import SimulationCard from "./SimulationCard";
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default class Workshop extends Component {
   constructor(props) {
