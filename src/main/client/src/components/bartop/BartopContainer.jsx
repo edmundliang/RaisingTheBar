df0e28306b085069a79fcfdbe9c6fc9d44c9323a
Edmund Liang <edmundliang95@gmail.com>
2019-12-18 01:19:07 -0500

    fixed cup contents for tooltip and recipe right panel

--- src/main/client/src/components/bartop/BartopContainer.jsx
+++ src/main/client/src/components/bartop/BartopContainer.jsx
@@ -729,8 +729,12 @@ export default class SimulationContainer extends Component {
                     return (<p key={item.name + index}>{item.name} {item.amount} ct</p>);
                   } else {
                     // if item is array ("shake" + shaken items)
-    
-                    return (<p>array</p>);
+                    return item[1].map((ingredient, index) => { 
+										
+			console.log(ingredient.name)
+			return <p id="log-text">{ingredient.name + " " + (ingredient.amount / 100) + " oz"}</p>
+										
+                    });
                     
                   }
   
